eccf67f97afb22eb9771cdb629027fce35b9bfce
usedcarsbuyer <desdemona849@gmail.com>
2025-10-12 08:00:34 -0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Typing effect
-const textArray = ["Creative Computing Graduate", "Web Developer", "Future Cybersecurity Expert"];
+const textArray = ["Creative Computing Graduate", "Web Developer", "Machine Learning Enthusiast", "Future Cybersecurity Expert"];
 let i = 0, j = 0, isDeleting = false, currentText = "";
 
 function type() {
@@ -18,7 +17,8 @@ function type() {
       i = (i + 1) % textArray.length;
     }
 
-    setTimeout(type, isDeleting ? 70 : 100);
+    // slower typing effect
+    setTimeout(type, isDeleting ? 120 : 180);
   }
 }
 window.onload = type;
